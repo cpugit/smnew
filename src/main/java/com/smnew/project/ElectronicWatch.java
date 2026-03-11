0182bb274e549be7a11aa5ec4057434e1f8b5cdf
roman miroshnychenko <romanmvp@gmail.com>
2026-03-11 22:52:57 +0100

    ElectronicWatch

---
+++ src/main/java/com/smnew/project/ElectronicWatch.java
@@ -0,0 +1,17 @@
+package main.java.com.smnew.project;
+
+import java.util.Scanner;
+
+public class ElectronicWatch {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int totalSeconds = scanner.nextInt();
+
+        int hours = (totalSeconds / 3600) % 24;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+
+        System.out.printf("%d:%02d:%02d", hours, minutes, seconds);
+    }
+}
---
+++ src/main/java/com/smnew/project/Main.java
@@ -0,0 +1,5 @@
+package main.java.com.smnew.project;
+
+public class Main {
+
+}
