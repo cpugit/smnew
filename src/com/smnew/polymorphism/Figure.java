ed3db8a8b32d762bd4b501ed36c1ded745a99bc4
roman miroshnychenko <romanmvp@gmail.com>
2026-06-22 23:29:43 +0200

    L

---
+++ src/com/smnew/polymorphism/Figure.java
@@ -0,0 +1,20 @@
+package com.smnew.polymorphism;
+
+public class Figure {
+
+    protected double dim1;
+    protected double dim2;
+
+    Figure (double dim1, double dim2){
+        this.dim1 = dim1;
+        this.dim2 = dim2;
+    }
+
+    public double area (){
+        System.out.println("Area of the figure not determined");
+        return 0.0;
+    }
+
+
+
+}
---
+++ src/com/smnew/polymorphism/Rectangle.java
@@ -0,0 +1,12 @@
+package com.smnew.polymorphism;
+
+public class Rectangle extends Figure {
+    Rectangle(double dim1, double dim2){
+        super(dim1, dim2);
+    }
+
+    public double area () {
+        System.out.println("Rectangle");
+        return dim1 * dim2;
+    }
+}
